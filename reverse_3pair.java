19a65ae7a5a5763ab10a224bd946192820433d64
jenishamary896 <jenishamary896@gmail.com>
2026-08-02 16:36:11 +0530

    Create reverse_3pair.java

---
+++ reverse_3pair.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n-2;i=i+3){
+            int temp=arr[i];
+            
+            arr[i]=arr[i+2];
+            
+            arr[i+2]=temp;
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
